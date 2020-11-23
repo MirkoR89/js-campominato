b415ace96d3aa63d2b0d01377cbfdb77c77fef0d
Mirko Rinelli <mirko.rinelli@gmail.com>
2020-11-23 16:31:19 +0100

    Generated request user numbers

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,24 @@
 //Il computer deve generare 16 numeri casuali tra 1 e 100.I numeri non possono essere duplicati.
 for (i=1; i<=16; i++) {
-  var pcNumbers = Math.floor(Math.random() * (100 - 1) + 1);
-  console.log(pcNumbers);
+  var pcNumber = Math.floor(Math.random() * (100 - 1) + 1);
+  console.log(pcNumber);
 }
-//In seguito deve chiedere all’utente (100 - 16) volte di inserire un numero alla volta, sempre compreso tra 1 e 100. L’utente non può inserire più volte lo stesso numero.
+listPcNumber = [];
+listPcNumber.push(pcNumber);
+console.log(listPcNumber);
 
+//In seguito deve chiedere all’utente di inserire un numero alla volta, sempre compreso tra 1 e 100. L’utente non può inserire più volte lo stesso numero.
+for (i=1; i<=16; i++) {
+  var userNumber = Number(prompt("Inserisci il tuo numero"))
+  console.log(userNumber);
+}
+listUserNumber = [];
+listUserNumber.push(userNumber);
+console.log(listUserNumber);
 //Se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti si continua chiedendo all’utente un altro numero.
+// if (userNumber == pcNumber) {
+//   console.log("You lose the match!");
+// }
 
 //La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
 
