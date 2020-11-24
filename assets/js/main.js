a083ec04d29b6c0c84f0d3c32f2b8215c6e156c1
Mirko Rinelli <mirko.rinelli@gmail.com>
2020-11-24 17:09:02 +0100

    Work in progress

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,51 @@
+//**************************** MY SOLUTION ****************************
+//Functions
+function randomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function includes(array, number) {
+  return array.includes(number);
+}
+
+function requestNumber() {
+  return Number(prompt("Inserisci un numero da 1 a 100"));
+}
+//Il computer deve generare 16 numeri casuali tra 1 e 100. I numeri non possono essere duplicati.
+
+var listPcNumber = [];
+
+while (listPcNumber.length < 16) {
+  var pcNumber = randomNumber(1, 100);
+  if (includes(listPcNumber, pcNumber) == false) {
+    listPcNumber.push(pcNumber);
+  }
+}
+console.log(listPcNumber);
+
+//In seguito deve chiedere all’utente di inserire un numero alla volta, sempre compreso tra 1 e 100. L’utente non può inserire più volte lo stesso numero.
+
+var listUserNumber = [];
+var probability = 100 - 16;
+
+while (listUserNumber.length < 5) {
+  var userNumber = requestNumber();
+  if (includes(listUserNumber, userNumber)) {
+    var wrongNumber = alert("Hai già usato questo numero! Inserisci un altro numero, premi ok e riprova.");
+    var userNumber = requestNumber();
+  } else if (includes(listUserNumber, userNumber) == false) {
+    listUserNumber.push(userNumber);
+//La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
+  } else if (includes(listPcNumber, userNumber)) {
+    var lose = document.getElementById("win_or_lose").innerHTML = "Game Over";
+  }
+}
+console.log(listUserNumber);
+//Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
+var win = document.getElementById("win_or_lose").innerHTML = "YOU WIN!";
+
+var score = document.getElementById("score").innerHTML = "Il tuo punteggio è" + " " + (listUserNumber.length);
+
 //************************** FABIO SOLUTION ***************************
 
 //Il computer deve generare 16 numeri casuali tra 1 e 100.I numeri non possono essere duplicati.
@@ -47,47 +95,3 @@ console.log(listUserNumber);
 
 //Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
 var score = document.getElementById("score").innerHTML = "Il tuo punteggio è" + " " + (listUserNumber.length);*/
-
-//**************************** MY SOLUTION ****************************
-//Functions
-function randomNumber(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-function includes(array, number) {
-  if (array.includes(number) == false) {
-    return array.push(number);
-  }
-}
-//Il computer deve generare 16 numeri casuali tra 1 e 100. I numeri non possono essere duplicati.
-
-listPcNumber = [];
-
-while (listPcNumber.length < 16) {
-  var pcNumber = randomNumber(1, 100);
-  includes(listPcNumber, pcNumber)
-}
-console.log(listPcNumber);
-
-//In seguito deve chiedere all’utente di inserire un numero alla volta, sempre compreso tra 1 e 100. L’utente non può inserire più volte lo stesso numero.
-
-
-// listUserNumber = [];
-// wrongNumber = listPcNumber.includes(userNumber);
-//
-// do {
-//   var userNumber = Number(prompt("Inserisci un numero da 1 a 100"));
-//
-//   if (wrongNumber == true) {
-//     console.log("Hai perso");
-//   } else if (listUserNumber.includes(userNumber) == false) {
-//     listUserNumber.push(userNumber);
-//   } else if (listUserNumber.includes(userNumber) == true) {
-//     console.log("Numero già inserito.");
-//   } else {
-//     console.log("Hai vinto!");
-//   }
-// } while (listUserNumber.length < 84 && wrongNumber == false)
-// console.log("il tuo punteggio è" + " " + listUserNumber.length);
-
-//********************************************************************
