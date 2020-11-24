4b96120dcac1d41e59ac453d22dcdd5837ea6a74
Mirko Rinelli <mirko.rinelli@gmail.com>
2020-11-24 20:40:01 +0100

    Bonus track (no complete)

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-//**************************** MY SOLUTION ****************************
+//**************************** MY SOLUTION ***************************
 //Functions
 function randomNumber(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -11,12 +11,28 @@ function includes(array, number) {
 function requestNumber() {
   return Number(prompt("Inserisci un numero da 1 a 100"));
 }
+// --------------------------- BONUS TRACK --------------------------
+var levelChoose = Number(prompt("Iserisci: 0 = Livello FACILE, 1 = Livello MEDIO, 2 = Livello DIFFICILE"));
+var levelEasy = 0;
+var levelMedium = 1;
+var levelHard = 2;
+
+if (levelChoose === levelEasy) {
+  var pcNumber = randomNumber(1, 100);
+  var probability = 100 - 16;
+} else if (levelChoose === levelMedium) {
+  var pcNumber = randomNumber(1, 80);
+  var probability = 80 - 16;
+} else if (levelChoose === levelHard) {
+  var pcNumber = randomNumber(1, 50);
+  var probability = 50 - 16;
+}
+// __________________________________________________________________
 //Il computer deve generare 16 numeri casuali tra 1 e 100. I numeri non possono essere duplicati.
 
 var listPcNumber = [];
 
 while (listPcNumber.length < 16) {
-  var pcNumber = randomNumber(1, 100);
   if (includes(listPcNumber, pcNumber) == false) {
     listPcNumber.push(pcNumber);
   }
@@ -24,13 +40,11 @@ while (listPcNumber.length < 16) {
 console.log(listPcNumber);
 
 //In seguito deve chiedere all’utente di inserire un numero alla volta, sempre compreso tra 1 e 100. L’utente non può inserire più volte lo stesso numero.
-//La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
-
 var listUserNumber = [];
-var probability = 100 - 16;
 
 while (listUserNumber.length < probability) {
   var userNumber = requestNumber();
+//La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
   if (includes(listUserNumber, userNumber)) {
     var wrongNumber = alert("Hai già usato questo numero! Inserisci un altro numero, premi ok e riprova.");
     var userNumber = requestNumber();
@@ -45,7 +59,8 @@ while (listUserNumber.length < probability) {
   var score = document.getElementById("score").innerHTML = "Il tuo punteggio è" + " " + (listUserNumber.length);
 }
 console.log(listUserNumber);
-//************************** FABIO SOLUTION ***************************
+
+//************************** FABIO SOLUTION **************************
 
 //Il computer deve generare 16 numeri casuali tra 1 e 100.I numeri non possono essere duplicati.
 /*function getRndInteger(min, max) {
