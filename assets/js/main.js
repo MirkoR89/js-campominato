21d4b065704a6cc805d2402d73ffd87f6c11f1b0
Mirko Rinelli <mirko.rinelli@gmail.com>
2020-11-24 17:33:01 +0100

    Bug fixed

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,28 +24,27 @@ while (listPcNumber.length < 16) {
 console.log(listPcNumber);
 
 //In seguito deve chiedere all’utente di inserire un numero alla volta, sempre compreso tra 1 e 100. L’utente non può inserire più volte lo stesso numero.
+//La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
 
 var listUserNumber = [];
 var probability = 100 - 16;
 
-while (listUserNumber.length < 5) {
+while (listUserNumber.length < probability) {
   var userNumber = requestNumber();
   if (includes(listUserNumber, userNumber)) {
     var wrongNumber = alert("Hai già usato questo numero! Inserisci un altro numero, premi ok e riprova.");
     var userNumber = requestNumber();
-  } else if (includes(listUserNumber, userNumber) == false) {
-    listUserNumber.push(userNumber);
-//La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
-  } else if (includes(listPcNumber, userNumber)) {
+  } else if (includes(listPcNumber, userNumber) == true) {
     var lose = document.getElementById("win_or_lose").innerHTML = "Game Over";
+    break;
+//Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
+  } else if (includes(listUserNumber, userNumber) == false) {
+      listUserNumber.push(userNumber);
+      var win = document.getElementById("win_or_lose").innerHTML = "YOU WIN!";
   }
+  var score = document.getElementById("score").innerHTML = "Il tuo punteggio è" + " " + (listUserNumber.length);
 }
 console.log(listUserNumber);
-//Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
-var win = document.getElementById("win_or_lose").innerHTML = "YOU WIN!";
-
-var score = document.getElementById("score").innerHTML = "Il tuo punteggio è" + " " + (listUserNumber.length);
-
 //************************** FABIO SOLUTION ***************************
 
 //Il computer deve generare 16 numeri casuali tra 1 e 100.I numeri non possono essere duplicati.
