ab9acf2d72a5bc3dfbbb0c90836090da8554479b
Mirko Rinelli <mirko.rinelli@gmail.com>
2020-11-24 12:47:21 +0100

    New code

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,57 @@
 //Il computer deve generare 16 numeri casuali tra 1 e 100.I numeri non possono essere duplicati.
-listPcNumber = [];
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+var listPcNumber = [];
+while (listPcNumber.length !== 16) {
+  var pcNumber = getRndInteger(1, 100);
+  if (! inArray(listPcNumber, pcNumber)) {
+    listPcNumber.push(pcNumber)
+  }
+}
+
+function inArray(array, number) {
+  var i = 0;
+  while (i < array.length) {
+    if (number === array[i]) {
+      return true;
+    }
+    i++
+  }
+}
+console.log(listPcNumber);
+
+//In seguito deve chiedere all’utente di inserire un numero alla volta, sempre compreso tra 1 e 100. L’utente non può inserire più volte lo stesso numero.
+
+var listUserNumber = [];
+var probability = 100 - 16;
+for (var i = 0; i <= probability; i++) {
+  var userNumber = Number(prompt("Inserisci un mumero tra 1 e 100"));
+  if (inArray(listPcNumber, userNumber)) {
+    console.log("Game Over!");
+    break;
+  }
+
+//Se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti si continua chiedendo all’utente un altro numero. La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
+
+  while (inArray(listUserNumber, userNumber)) {
+    alert("Hai già usato questo numero! Inserisci un altro numero, premi ok e riprova.");
+    var userNumber = Number(prompt("Inserisci un mumero tra 1 e 100"));
+  }
+  listUserNumber.push(userNumber);
+}
+console.log(listUserNumber);
+
+//Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
+
+
+
+console.log("Il tuo punteggio è" + " " + listUserNumber);
+
+//**************************** MY SOLUTION ****************************
+
+/*listPcNumber = [];
 
 while (listPcNumber.length < 16) {
   var pcNumber = Math.floor(Math.random() * (100 - 1) + 1);
@@ -9,14 +61,12 @@ while (listPcNumber.length < 16) {
 }
 console.log(listPcNumber);
 
-//In seguito deve chiedere all’utente di inserire un numero alla volta, sempre compreso tra 1 e 100. L’utente non può inserire più volte lo stesso numero.
 listUserNumber = [];
-
 wrongNumber = listPcNumber.includes(userNumber);
 
 do {
   var userNumber = Number(prompt("Inserisci un numero da 1 a 100"));
-//Se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti si continua chiedendo all’utente un altro numero. La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
+
   if (wrongNumber == true) {
     console.log("Hai perso");
   } else if (listUserNumber.includes(userNumber) == false) {
@@ -27,5 +77,6 @@ do {
     console.log("Hai vinto!");
   }
 } while (listUserNumber.length < 84 && wrongNumber == false)
-//Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
-console.log("il tuo punteggio è" + " " + listUserNumber.length);
+console.log("il tuo punteggio è" + " " + listUserNumber.length);*/
+
+//********************************************************************
